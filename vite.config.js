6a5050057c14a866f4febce8a2503f54cf5b4b34
narasimha-Demo <nellorenarasimharao333@gmail.com>
2026-08-31 12:29:02 +0530

    added photo and scroll animaiton in login and about pages...

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        about: resolve(__dirname, 'about.html'),
+      },
+    },
   },
   server: {
     port: 5173,
